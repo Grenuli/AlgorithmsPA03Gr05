7c0950858d973035460071b5976948a8300470c8
Lena <annalena.stohwasser@hotmail.de>
2017-11-30 01:55:22 +0100

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <time.h>
+#include <string>
+#include <sstream>
+#include <vector>
+
+#include "SkipList.h"
+
+
+using namespace std;
+//==================================================
+int main() {
+	vector<int64_t> startVector = { 1, 7, 4, 43, 45, 2, 9, 435, 5, 3, 8, 13, 11};
+
+
+	SkipList sl = SkipList(startVector);
+}
